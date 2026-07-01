62fd5bb95f6be7f9d5013ab77c18826fd3c1c403
YASH SANKPAL <yashsankpal2727@gmail.com>
2026-07-01 15:05:39 +0530

    Time: 23 ms (54.54%), Space: 17.4 MB (84.24%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> unique_quads;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                unordered_set<long long> seen;
-                for (int k = j + 1; k < n; k++) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k];
-                    long long fourth = (long long)target - sum;
-                    
-                    if (seen.count(fourth)) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
-                        unique_quads.insert(temp);
-                    }
-                    seen.insert(nums[k]);
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j!=i+1 && nums[j] ==nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+                    }else if(sum<target) k++;
+                    else l--;
                 }
             }
         }
-        
-        return vector<vector<int>>(unique_quads.begin(), unique_quads.end());
+        return ans;
     }
 };
